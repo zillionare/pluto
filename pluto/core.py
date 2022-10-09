c632119d50c6886c8409ba59781c457f19aaf548
aaron yang <aaron_yang@jieyu.ai>
2022-10-09 15:08:17 +0800

    📃 docs:

--- pluto/core.py
+++ pluto/core.py
@@ -12,13 +12,13 @@ from omicron.models.stock import Stock
 logger = logging.getLogger(__name__)
 
 
-async def score(bars: bars_dtype, code: str = None) -> Tuple:
+async def vanilla_score(bars: bars_dtype, code: str = None) -> Tuple:
     """对买入信号发出之后一段时间的表现进行评价。
 
     规则：
-        1. bars中的第一根bar为信号发出日。如果此时未涨停，则以收盘价作为买入价
+        1. bars中的第一根bar为信号发出日(T0)。如果此时未涨停，则以收盘价作为买入价
         2. 如果第一天收盘已涨停，则使用第二天的开盘价作为买入价
-        3. 以信号发出日为T0日，分别计算T1, T2, ..., T(len(bars)-1)日的累计涨跌幅
+        3. 分别计算T1, T2, ..., T(len(bars)-1)日的累计涨跌幅
         4. 计算累计最大涨幅
         5. 计算出现累计最大跌幅（上涨之前）
         6. 计算中除情况2之外，都使用收盘价计算。
