1147c795c457e11dc6c26b6d5378e0ad61b6cb92
Belvaaaan <552394134@qq.com>
2022-11-01 15:52:08 +0800

    ✨ feat:
    modified touch_buy_limit_pool

--- pluto/store/touch_buy_limit_pool.py
+++ pluto/store/touch_buy_limit_pool.py
@@ -2,12 +2,13 @@
 """
 import datetime
 import logging
+from types import FrameType
 from typing import Optional, Tuple
 
 import arrow
 import numpy as np
 from omicron import tf
-from omicron.extensions import find_runs
+from omicron.extensions import find_runs, price_equal
 from omicron.models.security import Security
 from omicron.models.stock import Stock
 
@@ -22,17 +23,21 @@ class TouchBuyLimitPoolStore(ZarrStore):
             ("name", "<U16"),
             ("code", "<U16"),
             ("date", "datetime64[s]"),
-            ("shadow", "f4"),
-            ("va", "f4"),
-            ("vb", "f4"),
+            ("upper_line", "f4"),
         ]
     )
 
-    def __init__(self, path: str = None):
+    def __init__(self, path: str = "传入一个path,用于测试"):
         super().__init__(path)
 
     def save(self, records):
         self._store.append(records)
+
+    def _adjust_timestamp(self, timestamp: datetime.date) -> datetime.date:
+        if tf.is_trade_day(timestamp) and datetime.datetime.now().hour < 15:
+            return tf.day_shift(timestamp, -1)
+        else:
+            return tf.day_shift(timestamp, 0)
 
     async def get(self, timestamp: datetime.date):
         start = tf.combine_time(timestamp, 0)
@@ -40,44 +45,36 @@ class TouchBuyLimitPoolStore(ZarrStore):
         idx = np.argwhere((self._store["date"] >= start) & (self._store["date"] < end))
         return self._store[idx]
 
-    async def extract_features(
-        self, code: str, end: datetime.date, n: int = 10
+    async def extract_touch_buy_limit_features(
+        self, code: str, end: datetime.date
     ) -> Optional[Tuple]:
-        """提取个股在[start, end]期间的涨跌停特征
+        """提取个股在[end]期间冲涨停特征, 只记录当天第一次涨停时间
 
         Args:
             code: 股票代码
             end: 截止时间
-            n: 取多少个bar进行检查
 
         Returns:
-            如果存在涨停，则返回(name, code, 总涨停次数，连续涨停次数，最后涨停时间，最后涨停距现在的bar数)
+            如果存在涨停，则返回(name, code, 涨停时间(30MIN为单位), 上引线百分比)
         """
-        start = tf.day_shift(end, -1)
 
         try:
-            flags = await Stock.get_trade_price_limits(code, start, end)
+            flags = await Stock.get_trade_price_limits(code, end, end)
             if flags is None or len(flags) == 0:
                 return None
 
-            flags, _ = flags
-            total = np.count_nonzero(flags)
+            high_limit = flags["high_limit"][0]
+            end_time = tf.combine_time(end, 15)
+            bar = await Stock.get_bars(code, 8, FrameType.MIN30, end_time)
+            close = bar["close"][-1]
+            open = bar["close"][-1]
+            for i in range(len(bar)):
+                limit_signal = price_equal(bar["high"][i], high_limit)
+                if limit_signal and (not price_equal(close, high_limit)):
+                    name = await Security.alias(code)
+                    upper_line = high_limit / max(close, open) - 1
+                    return (name, code, bar["frame"][i].item(), upper_line)
 
-            if total > 0:
-                name = await Security.alias(code)
-                if name.find("ST") != -1:
-                    return None
-
-                last = np.argwhere(flags).ravel()[-1]
-                last_date = tf.day_shift(end, last - n + 1)
-
-                v, _, length = find_runs(flags)
-                pos = np.where(v == 1)
-                cont = length[pos]
-                cont = max(cont)
-                till_now = n - last
-
-                return name, code, total, cont, last_date, till_now
         except Exception as e:
             logger.exception(e)
 
@@ -85,7 +82,7 @@ class TouchBuyLimitPoolStore(ZarrStore):
         if end is None:
             end = self._adjust_timestamp(arrow.now().date())
 
-        logger.info("building buy limit pool for %s(%s)...", end, self.win)
+        logger.info("building buy limit pool on %s...", end)
         secs = (
             await Security.select()
             .types(["stock"])
@@ -96,7 +93,7 @@ class TouchBuyLimitPoolStore(ZarrStore):
         )
         result = []
         for sec in secs:
-            r = await self.extract_buy_limit_features(sec, end, self.win)
+            r = await self.extract_touch_buy_limit_features(sec, end)
             if r is not None:
                 result.append(r)
 
